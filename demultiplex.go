99f08419210c09225ae01cfcda229f4a4ad5e229
oliver <oliver@localhost.localdomain>
2018-06-27 16:37:10 -1000

    output path

--- demultiplex.go
+++ demultiplex.go
@@ -319,8 +319,10 @@ func launchAnalysisMultipleFile() {
 		strings.TrimSuffix(filenameR2, ".fastq.bz2"),
 		OUTPUT_TAG_NAME)
 
-	cmd1 := fmt.Sprintf("cat index_*demultiplexed.repl1.fastq.bz2 > %s", outputR1)
-	cmd2 := fmt.Sprintf("cat index_*demultiplexed.repl1.fastq.bz2 > %s", outputR2)
+	cmd1 := fmt.Sprintf("cat %sindex_*demultiplexed.repl1.fastq.bz2 > %s",
+		OUTPUT_PATH, outputR1)
+	cmd2 := fmt.Sprintf("cat %sindex_*demultiplexed.repl1.fastq.bz2 > %s",
+		OUTPUT_PATH, outputR2)
 
 	fmt.Printf("concatenating read 1 files...\n")
 	fmt.Printf("%s\n", cmd1)
